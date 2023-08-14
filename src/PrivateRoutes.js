4d2c134342ad69aa33ea6ed4a650a857d23e5760
abisheg0213 <109604992+abisheg0213@users.noreply.github.com>
2023-08-14 19:01:00 +0530

    Update PrivateRoutes.js

--- src/PrivateRoutes.js
+++ src/PrivateRoutes.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { Outlet, Navigate } from "react-router-dom";
 import { auth } from "./firebase_config";
+import { useSelector, useDispatch } from "react-redux";
 import { onAuthStateChanged } from "firebase/auth";
 export const PrivateRoutes = () => {
-  console.log(auth);
-  console.log(auth.currentUser === null);
-  let auuser = true;
-  return auth.currentUser !== null ? <Outlet /> : <Navigate to="/login" />;
+  const status = useSelector((state) => state.counter);
+  return status ? <Outlet /> : <Navigate to="/login" />;
 };
